38d21c7a03f1998543c0031a80bdfaa89d996c1d
Daniel Nogales Mateos <160231901+daninogales30@users.noreply.github.com>
2026-07-02 00:09:56 +0200

    Simplify Vite config by removing plugins
    Removed React and Babel plugins from Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  base: '/daninogales30-portfolio.github.io/'
 })
